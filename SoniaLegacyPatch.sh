979f21aca936f1c9ee40b947d22b9ef06a152386
Allison <108936256+ArchiverXP@users.noreply.github.com>
2024-10-21 19:41:40 -0400

    Made the script more easy to use I think.

--- SoniaLegacyPatch.sh
+++ SoniaLegacyPatch.sh
@@ -2,31 +2,32 @@
 set -e
 
 if [[ -z "$1" ]]; then
-  echo "Usage: $0 /path/to/decrypted.ipa appname"
+  echo "Usage: $0 appname_payload_extracted"
   exit 1
 fi
 
-unzip $1 Payload/
-
 #P.S: You are free to change the MininumOSVersion to whatever you'd like, only iOS 6 is supported though, so don't expect support.
-/usr/libexec/PlistBuddy -c "set :MinimumOSVersion 6.0" Payload/$2.app/Info.plist
-if cp .theos/obj/debug/GameController.dylib Payload/$2.app/Frameworks/; then
+/usr/libexec/PlistBuddy -c "set :MinimumOSVersion 6.0" Payload/$1.app/Info.plist
+if cp .theos/obj/debug/GameController.dylib  libsystem_m_mod.dylib Payload/$1.app/Frameworks/; then
     echo "Copying DYLIB to Frameworks folder."
 else
-    mkdir Payload/$2.app/Frameworks/
-    cp .theos/obj/debug/GameController.dylib Payload/$2.app/Frameworks/
+    mkdir Payload/$1.app/Frameworks/
+    cp .theos/obj/debug/GameController.dylib libsystem_m_mod.dylib Payload/$1.app/Frameworks/
 fi
 
 install_name_tool \
-    -change /System/Library/Frameworks/GameController.framework/GameController @executable_path/Frameworks/GameController.dylib Payload/$2.app/$2 \
-    #-change /System/Library/Frameworks/Foundation.framework/Foundation @executable_path/Frameworks/Foundation.dylib \
+    -change /System/Library/Frameworks/GameController.framework/GameController @executable_path/Frameworks/GameController.dylib Payload/$1.app/$1 \
+    -change /usr/lib/libSystem.B.dylib @executable_path/Sonia/ReSoniaDylib.dylib \
 
+#Is this needed?
+#./optool \
+#    install -c reexport -p @executable_path/Frameworks/libsystem_m_mod.dylib -t $1 -o Payload/$1/
 
 zip -r "$1" Payload
 rm Payload/
 
 #LEFTOVER:
 #install_name_tool \
-#  -change /usr/lib/libSystem.B.dylib @executable_path/Frameworks/Sonia.dylib Payload/$2.app/$2 \
+#  -change /usr/lib/libSystem.B.dylib @executable_path/Frameworks/Sonia.dylib Payload/$1.app/$1 \
 #  -add_rpath /usr/lib/libSystem.B.dylib \
 
